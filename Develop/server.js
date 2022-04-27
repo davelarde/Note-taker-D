6619f9238c300601953f50c9d9944bce5cfa3557
danivelarde <danielavelarde4@gmail.com>
2022-04-27 17:54:33 -0400

    setting up routes

--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,10 @@
 const express = require ("express");
 
 const app = express()
+const path = require ("path")
+const fs= require("fs")
+const util = require ("util")
+const uuid = require("./helpers/uuid")
 const PORT = process.env.PORT || 3000;
 //  set up to handle data parsing
 
@@ -11,4 +15,10 @@ app.use(express.json());
 //  this serves static files in public directories
 app.use(express.static("public"));
 
-require ("./db")
+require("./Routes/apiRoutes")(app);
+require ("./Routes/htmlRoutes")(app);
+
+// begin listening
+app.listen(PORT,function(){
+    console.log("App listening on http://localhost:" + PORT);
+});
