900d5050333bbfe14d6fa56787eea921c38b7196
danivelarde <danielavelarde4@gmail.com>
2022-04-27 19:57:23 -0400

    changes in server js

--- Develop/server.js
+++ Develop/server.js
@@ -1,18 +1,16 @@
-// create dependencies and set up express app
+
 const express = require ("express");
 
 const app = express()
 
+const dbJason =require("./db/db.json")
 
-const util = require ("util")
-const uuid = require("./helpers/uuid")
 const PORT = process.env.PORT || 3000;
-//  set up to handle data parsing
 
 app.use(express.urlencoded({ extended : true}));
 app.use(express.json());
 
-//  this serves static files in public directories
+
 app.use(express.static("public"));
 
 const apiRoutes= require("./Routes/apiRoutes");
@@ -20,7 +18,7 @@ const HtmlRoutes = require ("./Routes/htmlRoutes");
 app.use("/api", apiRoutes);
 app.use("/", HtmlRoutes);
 
-// begin listening
+
 app.listen(PORT,function(){
     console.log("App listening on http://localhost:" + PORT);
 });
